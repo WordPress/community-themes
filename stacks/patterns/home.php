15fed08e188c183f7b45e3a264f87dab5a850780
scruffian <ben@scruffian.com>
2023-03-15 17:20:32 +0000

    escape the home patterns

--- stacks/patterns/home.php
+++ stacks/patterns/home.php
@@ -12,7 +12,7 @@
 <div class="wp-block-group">
 
 <!-- wp:heading {"level":1,"fontSize":"x-large"} -->
-<h1 class="wp-block-heading has-x-large-font-size">Create Stacks.</h1>
+<h1 class="wp-block-heading has-x-large-font-size"><?php echo esc_html__( 'Create Stacks.', 'stacks' ); ?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -24,7 +24,7 @@
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">A simple theme for telling simple stories</h2>
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'A simple theme for telling simple stories', 'stacks' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -36,7 +36,7 @@
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Treat your site like a presentation.</h2>
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'Treat your site like a presentation.', 'stacks' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -48,7 +48,7 @@
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">It should be easy to follow,</h2>
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'It should be easy to follow,', 'stacks' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -60,7 +60,7 @@
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">convey your brand,</h2>
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'convey your brand,', 'stacks' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -72,7 +72,7 @@
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"center","textColor":"white","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size">and most importantly, convert well.</h2>
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size"><?php echo esc_html__( 'and most importantly, convert well.', 'stacks' ); ?></h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -81,30 +81,30 @@
 <div class="wp-block-group has-background" style="background-color:#0e0e0e;min-height:100vh"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/stack.jpg","id":29,"dimRatio":50,"focalPoint":{"x":0,"y":0.5},"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
 <div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-29" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/stack.jpg" style="object-position:0% 50%" data-object-fit="cover" data-object-position="0% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","textColor":"white","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size">Create your own Stacks</h2>
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size"><?php echo esc_html__( 'Create your own Stacks', 'stacks' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:list {"ordered":true,"textColor":"white"} -->
 <ol class="has-white-color has-text-color"><!-- wp:list-item -->
-<li>Add the "stacks" pattern to a new post.</li>
+<li><?php echo esc_html__( 'Add the "Stacks" pattern to a new post.', 'stacks' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Update the content.</li>
+<li><?php echo esc_html__( 'Update the content.', 'stacks' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Publish your post and share it.</li>
+<li><?php echo esc_html__( 'Publish your post and share it.', 'stacks' ); ?></li>
 <!-- /wp:list-item --></ol>
 <!-- /wp:list -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/theme/stacks">Get the Stacks theme</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/theme/stacks"><?php echo esc_html__( 'Get the Stacks theme', 'stacks' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
 <!-- wp:paragraph {"textColor":"white","className":"has-text-color","fontSize":"small"} -->
-<p class="has-text-color has-white-color has-small-font-size">Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>.</p>
+<p class="has-text-color has-white-color has-small-font-size"><?php echo esc_html__( 'Proudly Powered by', 'stacks' ); ?><a href="https://wordpress.org" rel="nofollow"><?php echo esc_html__( 'WordPress', 'stacks' ); ?></a><?php echo esc_html__( '.', 'stacks' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover --></div>
