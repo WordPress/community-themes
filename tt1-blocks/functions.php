bdb9cda94baa7d8bb83664a67dc226adf362fcae
MaggieCabrera <maggie.cabrera@automattic.com>
2023-03-23 16:53:29 +0100

    copied from origin repo

---
+++ tt1-blocks/functions.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package TT1_Blocks
+ * @since 0.1
+ */
+
+if ( ! function_exists( 'tt1_blocks_setup' ) ) {
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 0.1
+	 *
+	 * @return void
+	 */
+	function tt1_blocks_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on TT1 Blocks, use a find and replace
+		 * to change 'tt1-blocks' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'tt1-blocks', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1568, 9999 );
+
+		// Add support for Block Styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style(
+			array(
+				'./assets/css/blocks.css',
+				'./assets/css/style-shared.css',
+			)
+		);
+
+		// Add support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
+	}
+}
+add_action( 'after_setup_theme', 'tt1_blocks_setup' );
+
+/**
+ * Enqueue scripts and styles.
+ *
+ * @since 0.1
+ *
+ * @return void
+ */
+function tt1_blocks_enqueue_styles() {
+	wp_enqueue_style( 'tt1-blocks-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'tt1-blocks-style-shared', get_template_directory_uri() . '/assets/css/style-shared.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'tt1-blocks-blocks', get_template_directory_uri() . '/assets/css/blocks.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'tt1_blocks_enqueue_styles' );
+
+/**
+ * Enqueue block editor script.
+ *
+ * @since 0.1
+ *
+ * @return void
+ */
+function tt1_blocks_block_editor_script() {
+
+	wp_enqueue_script( 'tt1-blocks-unregister-block-style', get_theme_file_uri( '/assets/js/unregister-block-style.js' ), array( 'wp-blocks', 'wp-dom' ), wp_get_theme()->get( 'Version' ), true );
+}
+add_action( 'enqueue_block_editor_assets', 'tt1_blocks_block_editor_script' );
+
+// Block Patterns.
+require get_template_directory() . '/inc/block-patterns.php';
+
+// Block Styles.
+require get_template_directory() . '/inc/block-styles.php';
---
+++ tt1-blocks/inc/block-styles.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Block Styles
+ *
+ * @link https://developer.wordpress.org/reference/functions/register_block_style/
+ *
+ * @package TT1_Blocks
+ * @since 0.1
+ */
+
+if ( function_exists( 'register_block_style' ) ) {
+	/**
+	 * Register block styles.
+	 *
+	 * @since 0.1
+	 *
+	 * @return void
+	 */
+	function twenty_twenty_one_register_block_styles() {
+		// Columns: Overlap.
+		register_block_style(
+			'core/columns',
+			array(
+				'name'  => 'twentytwentyone-columns-overlap',
+				'label' => esc_html__( 'Overlap', 'tt1-blocks' ),
+			)
+		);
+
+		// Cover: Borders.
+		register_block_style(
+			'core/cover',
+			array(
+				'name'  => 'twentytwentyone-border',
+				'label' => esc_html__( 'Borders', 'tt1-blocks' ),
+			)
+		);
+
+		// Image: Borders.
+		register_block_style(
+			'core/image',
+			array(
+				'name'  => 'twentytwentyone-border',
+				'label' => esc_html__( 'Borders', 'tt1-blocks' ),
+			)
+		);
+
+		// Image: Frame.
+		register_block_style(
+			'core/image',
+			array(
+				'name'  => 'twentytwentyone-image-frame',
+				'label' => esc_html__( 'Frame', 'tt1-blocks' ),
+			)
+		);
+
+		// Latest Posts: Dividers.
+		register_block_style(
+			'core/latest-posts',
+			array(
+				'name'  => 'twentytwentyone-latest-posts-dividers',
+				'label' => esc_html__( 'Dividers', 'tt1-blocks' ),
+			)
+		);
+
+		// Latest Posts: Borders.
+		register_block_style(
+			'core/latest-posts',
+			array(
+				'name'  => 'twentytwentyone-latest-posts-borders',
+				'label' => esc_html__( 'Borders', 'tt1-blocks' ),
+			)
+		);
+
+		// Media & Text: Borders.
+		register_block_style(
+			'core/media-text',
+			array(
+				'name'  => 'twentytwentyone-border',
+				'label' => esc_html__( 'Borders', 'tt1-blocks' ),
+			)
+		);
+
+		// Separator: Thick.
+		register_block_style(
+			'core/separator',
+			array(
+				'name'  => 'twentytwentyone-separator-thick',
+				'label' => esc_html__( 'Thick', 'tt1-blocks' ),
+			)
+		);
+	}
+	add_action( 'init', 'twenty_twenty_one_register_block_styles' );
+}
