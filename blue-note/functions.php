7ed61bd459184b60053e40274aed2e354bb822c9
Carolina Nymark <hi@themesbycarolina.com>
2023-03-24 10:05:28 +0100

    Add custom block styles for images

--- blue-note/functions.php
+++ blue-note/functions.php
@@ -1,22 +1,44 @@
 <?php
 /**
  * Register block styles.
- * TODO: Add CSS, repeat for both featured image and image blocks
  */
-register_block_style(
-	'core/image',
-	array(
-		'name'         => 'slant-left',
-		'label'        => __( 'Slant left', 'blue-note' ),
-		'inline_style' => '',
-	)
-);
+function blue_note_register_block_styles() {
+	$blocks = array( 'image', 'post-featured-image' );
 
-register_block_style(
-	'core/image',
-	array(
-		'name'         => 'slant-right',
-		'label'        => __( 'Slant right', 'blue-note' ),
-		'inline_style' => '',
-	)
-);
+	foreach( $blocks as $block ) {
+		register_block_style(
+			'core/' . $block,
+			array(
+				'name'         => 'blue-note-slant-right',
+				'label'        => __( 'Slant right', 'blue-note' ),
+				'inline_style' => '.is-style-blue-note-slant-right{ transform: rotate(1deg); }',
+			)
+		);
+		register_block_style(
+			'core/' . $block,
+			array(
+				'name'         => 'blue-note-slant-left',
+				'label'        => __( 'Slant left', 'blue-note' ),
+				'inline_style' => '.is-style-blue-note-slant-left{ transform: rotate(-1deg); }',
+			)
+		);
+	}
+
+	register_block_style(
+		'core/query',
+		array(
+			'name'  => 'blue-note-query-slant',
+			'label' => __( 'Slanted images', 'blue-note' ),
+			'inline_style' => '
+				.is-style-blue-note-query-slant ul li:nth-child(even) .wp-block-post-featured-image {
+					transform: rotate(-1deg);
+				}
+				.is-style-blue-note-query-slant ul li:nth-child(odd) .wp-block-post-featured-image {
+					transform: rotate(1deg);
+				}
+			',
+		)
+	);
+}
+add_action( 'init', 'blue_note_register_block_styles' );
+
