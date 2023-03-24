893d4fb15f701b223a57670fc9adff293d3d4e39
Saxon Fletcher <saxonafletcher@gmail.com>
2023-03-24 13:54:44 +1000

    update template and default styling

--- stacks/patterns/home.php
+++ stacks/patterns/home.php
@@ -3,109 +3,70 @@
  * Title: Stacks
  * Slug: stacks/home
  * Block Types: core/post-content
- * Post Types: post, page
  * Categories: featured
  */
 ?>
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-white-background-color has-background" style="min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
 
-<!-- wp:heading {"level":1,"fontSize":"x-large"} -->
-<h1 class="wp-block-heading has-x-large-font-size"><?php echo esc_html__( 'Create Stacks.', 'stacks' ); ?></h1>
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}},"border":{"radius":"25px"}},"backgroundColor":"primary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="border-radius:25px;min-height:100vh;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">This is a Stack</h1>
 <!-- /wp:heading --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#f6f6f6"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-background" style="background-color:#f6f6f6;min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"center","id":14,"width":24,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/tip.png" alt="" class="wp-image-14" width="24" height="24"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'A simple theme for telling simple stories', 'stacks' ); ?></h2>
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}},"border":{"radius":"25px"}},"backgroundColor":"secondary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group has-secondary-background-color has-background" style="border-radius:25px;min-height:100vh;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">A pattern to tell compelling stories in a simple way</h2>
 <!-- /wp:heading --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#eeeeee"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-background" style="background-color:#eeeeee;min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"center","id":15,"width":24,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Frame-3.png" alt="" class="wp-image-15" width="24" height="24"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'Treat your site like a presentation.', 'stacks' ); ?></h2>
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}},"border":{"radius":"25px"}},"backgroundColor":"tertiary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:25px;min-height:100vh;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">Create sites that feel like presentation decks</h2>
 <!-- /wp:heading --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#dddddd"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-background" style="background-color:#dddddd;min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"center","id":16,"width":24,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-list.png" alt="" class="wp-image-16" width="24" height="24"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'It should be easy to follow,', 'stacks' ); ?></h2>
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}},"border":{"radius":"25px"}},"backgroundColor":"primary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="border-radius:25px;min-height:100vh;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">They are easy to follow and lead to a clear call to action</h2>
 <!-- /wp:heading --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#cccccc"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-background" style="background-color:#cccccc;min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"center","id":23,"width":24,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/styles-2.png" alt="" class="wp-image-23" width="24" height="24"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html__( 'convey your brand,', 'stacks' ); ?></h2>
-<!-- /wp:heading --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#aaaaaa"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group has-background" style="background-color:#aaaaaa;min-height:100vh"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"center","id":24,"width":24,"height":24,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/currency-dollar-1.png" alt="" class="wp-image-24" width="24" height="24"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"textAlign":"center","textColor":"white","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size"><?php echo esc_html__( 'and most importantly, convert well.', 'stacks' ); ?></h2>
-<!-- /wp:heading --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"color":{"background":"#0e0e0e"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"stretch"}} -->
-<div class="wp-block-group has-background" style="background-color:#0e0e0e;min-height:100vh"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/stack.jpg","id":29,"dimRatio":50,"focalPoint":{"x":0,"y":0.5},"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
-<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-29" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/stack.jpg" style="object-position:0% 50%" data-object-fit="cover" data-object-position="0% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","textColor":"white","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-x-large-font-size"><?php echo esc_html__( 'Create your own Stacks', 'stacks' ); ?></h2>
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}},"border":{"radius":"25px"}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background" style="border-radius:25px;min-height:100vh;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textColor":"base","fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-base-color has-text-color has-x-large-font-size">Create your own Stack</h2>
 <!-- /wp:heading -->
 
-<!-- wp:list {"ordered":true,"textColor":"white"} -->
-<ol class="has-white-color has-text-color"><!-- wp:list-item -->
-<li><?php echo esc_html__( 'Add the "Stacks" pattern to a new post.', 'stacks' ); ?></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><?php echo esc_html__( 'Update the content.', 'stacks' ); ?></li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li><?php echo esc_html__( 'Publish your post and share it.', 'stacks' ); ?></li>
-<!-- /wp:list-item --></ol>
-<!-- /wp:list -->
+<!-- wp:paragraph {"fontSize":"md"} -->
+<p class="has-md-font-size">Download and activate the theme, insert the Stack pattern, and create as many slides as you want.</p>
+<!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/theme/stacks"><?php echo esc_html__( 'Get the Stacks theme', 'stacks' ); ?></a></div>
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"contrast"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-contrast-color has-base-background-color has-text-color has-background wp-element-button" href="https://wordpress.org/theme/stacks">Download theme</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
-<!-- wp:paragraph {"textColor":"white","className":"has-text-color","fontSize":"small"} -->
-<p class="has-text-color has-white-color has-small-font-size"><?php echo esc_html__( 'Proudly Powered by', 'stacks' ); ?><a href="https://wordpress.org" rel="nofollow"><?php echo esc_html__( 'WordPress', 'stacks' ); ?></a><?php echo esc_html__( '.', 'stacks' ); ?></p>
+
+<!-- wp:paragraph {"fontSize":"sm"} -->
+<p class="has-sm-font-size">Proudly Powered by<a rel="nofollow" href="https://wordpress.org">WordPress</a>.</p>
 <!-- /wp:paragraph --></div>
-<!-- /wp:group --></div></div>
-<!-- /wp:cover --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
