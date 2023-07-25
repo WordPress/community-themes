76e787b56f7a5e728821024ed6527d0948ca016a
Carolina Nymark <hi@themesbycarolina.com>
2023-07-25 14:46:11 +0200

    Create a hidden pattern for the comments area

---
+++ blue-note/patterns/hidden-comments.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Hidden comments
+ * Slug: blue-note/hidden-comments
+ * Categories: uncategorized
+ */
+?>
+<!-- wp:comments -->
+<div class="wp-block-comments">
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+<!-- wp:post-comments-form /-->
+
+<!-- wp:comments-title {"showPostTitle":false,"showCommentsCount":false} /-->
+
+<!-- wp:comment-template -->
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
+<!-- wp:comment-author-name /-->
+<!-- wp:comment-date /-->
+<!-- wp:comment-edit-link /-->
+</div>
+<!-- /wp:group -->
+<!-- wp:comment-content /-->
+<!-- wp:comment-reply-link /-->
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
+<div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+<!-- /wp:comment-template -->
+
+<!-- wp:comments-pagination {"paginationArrow":"arrow"} -->
+<!-- wp:comments-pagination-previous /-->
+
+<!-- wp:comments-pagination-numbers /-->
+
+<!-- wp:comments-pagination-next /-->
+<!-- /wp:comments-pagination --></div>
+<!-- /wp:group --></div>
+<!-- /wp:comments -->
