b049cb89c5f7a62ce70f0d5b95a4aa4714cfe5d1
Dave Smith <getdavemail@gmail.com>
2023-07-26 10:06:32 +0100

    Blue note - update 404 template (#70)
    * Add min height to search group
    
    * Add label text and hide label
    
    * Actually hide the hidden pattern
    
    * separated heading from header and changed typography for search block
    
    * changed layout alignments to use content size instead of static values
    
    ---------
    
    Co-authored-by: MaggieCabrera <maggie.cabrera@automattic.com>

--- blue-note/patterns/hidden-404.php
+++ blue-note/patterns/hidden-404.php
@@ -3,20 +3,22 @@
  * Title: Hidden 404
  * Slug: blue-note/hidden-404
  * Categories: uncategorized
+ * Inserter: no
  */
 ?>
-<!-- wp:group {"tagName":"main","layout":{"type":"constrained","justifyContent":"left","contentSize":"1320px"}} -->
-<main class="wp-block-group"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading"><?php esc_html_e( 'This page could not be found','blue-note' ); ?></h1>
+<!-- wp:group {"tagName":"main","layout":{"type":"constrained"}} -->
+<main class="wp-block-group">
+<!-- wp:heading {"level":1,"align":"full","style":{"spacing":{"margin":{"top":"6.25rem"},"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<h1 class="wp-block-heading alignfull" style="margin-top:6.25rem;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">This page could not be found</h1>
 <!-- /wp:heading -->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"60px","top":"59px"}}},"layout":{"type":"constrained","contentSize":"650px"}} -->
 <div class="wp-block-group alignfull" style="padding-top:59px;padding-bottom:60px"><!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"0px"}},"typography":{"fontSize":"22px"}}} -->
-<p style="padding-top:0px;font-size:22px"><?php esc_html_e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.','blue-note' ); ?></p>
+<p style="padding-top:0px;font-size:22px"><?php esc_html_e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'blue-note' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"60px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:60px"><!-- wp:search {"label":"","placeholder":"Search...","width":650,"buttonText":"Search","style":{"border":{"width":"1px"}},"borderColor":"contrast","backgroundColor":"contrast","className":"search_field"} /--></div>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"60px"}},"dimensions":{"minHeight":"25vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="min-height:25vh;padding-top:60px"><!-- wp:search {"label":"<?php esc_html_e( 'Search the site' ); ?>","showLabel":false,"placeholder":"Search...","width":650,"buttonText":"Search","style":{"border":{"width":"1px"}},"borderColor":"contrast","backgroundColor":"contrast","className":"search_field"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></main>
 <!-- /wp:group -->
