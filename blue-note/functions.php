fddd62dbb424d1288ff6825ece0a0472df789f3d
MaggieCabrera <maggie.cabrera@automattic.com>
2023-07-26 18:10:49 +0200

    aded thick stripes pattern and made changes to the svgs

--- blue-note/functions.php
+++ blue-note/functions.php
@@ -30,6 +30,14 @@ function blue_note_register_block_styles() {
 				'inline_style' => '.is-style-blue-note-thin-stripes{ clip-path: url('. esc_url( get_stylesheet_directory_uri() ) .'/assets/images/thin-stripes.svg#blue-note-thin-stripes); }',
 			)
 		);
+		register_block_style(
+			'core/' . $block,
+			array(
+				'name'         => 'blue-note-thick-stripes',
+				'label'        => __( 'Thick Stripes', 'blue-note' ),
+				'inline_style' => '.is-style-blue-note-thick-stripes{ clip-path: url('. esc_url( get_stylesheet_directory_uri() ) .'/assets/images/thick-stripes.svg#blue-note-thick-stripes); }',
+			)
+		);
 	}
 
 	register_block_style(
