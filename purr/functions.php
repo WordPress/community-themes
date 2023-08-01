1fb9444259240304d8e717df634105765f69a4fb
scruffian <ben@scruffian.com>
2023-08-01 17:58:27 +0100

    update tags

--- purr/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function purr_setup() {
-	add_theme_support( 'post-formats', array( 'status', 'gallery', 'aside', 'video', 'audio', 'image', 'link' ) );
-}
-
-add_action( 'after_setup_theme', 'purr_setup' );
