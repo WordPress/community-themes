9f7e5dd89327cc7d426f94b5b82efea1b6974773
Maggie <maggie.cabrera@automattic.com>
2023-08-08 11:49:58 +0200

    added pattern media text (#80)

---
+++ blue-note/patterns/media-text.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Image and text
+ * Slug: blue-note/media-text
+ * Categories: text
+ * Block Types: core/media-text
+ */
+?>
+<!-- wp:media-text {"align":"wide","mediaId":146,"mediaLink":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/John_Coltrane.png","mediaType":"image","mediaWidth":41,"verticalAlignment":"top","imageFill":true,"style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top is-image-fill" style="padding-right:0;padding-left:0;grid-template-columns:41% auto"><figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/John_Coltrane.png);background-position:50% 50%"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/John_Coltrane.png" alt="" class="wp-image-146 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:group {"style":{"spacing":{"padding":{"left":"var:preset|spacing|70","right":"0","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e( 'Pivotal Moments', 'blue-note' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'When Coltrane was only 12 years old, his father, aunt, and grandparents all died in quick succession, leaving only his mother to take care of him. At 17, Coltrane begin playing the alto saxophone, and, after serving time in the Navy, people begin to take notice of his abilities. Eventually, in 1955, Coltrane joined Miles Davis and three other musicians to form what is known as the “First Great Quintet,” which released four successful albums in the span of around 18 months.', 'blue-note' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'However, Coltrane had yet to reach his full potential, suffering from addiction to alcohol and heroin for most of his life after serving in the Navy. Due to Coltrane’s violation of Davis’ rule barring the use of alcohol and narcotics, he was kicked out of the quintet. Determined to kick this habit, Coltrane went cold turkey, completely stopping his use of drugs, a decision which almost killed him. After moving past his addiction, Coltrane rejoined Miles Davis in a sextet, which, over two years, released several critically acclaimed records.', 'blue-note' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'Then, Coltrane left to lead his own band, releasing the album, Giant Steps. The album, which was entirely composed by Coltrane, was revolutionary, and within it, Coltrane used a technique for chord progressions which had never been used before, hence it being named a “Coltrane change.”', 'blue-note' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:media-text -->
