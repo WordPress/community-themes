9a86ed8d2ce4fda09e68777c646d422f166c4356
scruffian <ben@scruffian.com>
2024-01-04 10:27:02 +0000

    update archives

--- atlas/patterns/posts-list.php
+++ atlas/patterns/posts-list.php
@@ -9,14 +9,14 @@
 <!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"wide","layout":{"type":"default"}} -->
 <div class="wp-block-query alignwide">
 	<!-- wp:post-template {"align":"wide"} -->
-		<!-- wp:post-title {"textAlign":"center","isLink":true,"align":"wide"} /-->
-		<!-- wp:pattern {"slug":"atlas/post-meta"} /-->
-		<!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"clamp(15vw, 30vh, 400px)","align":"wide"} /-->
-
 		<!-- wp:spacer {"height":"var:preset|spacing|50"} -->
 		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
 		<!-- /wp:spacer -->
-		
+
+		<!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"clamp(15vw, 30vh, 400px)","align":"wide","style":{"color":{"duotone":"var:preset|duotone|contrast"}}} /-->
+		<!-- wp:post-title {"textAlign":"center","isLink":true,"align":"wide"} /-->
+		<!-- wp:pattern {"slug":"atlas/post-meta"} /-->
+
 		<!-- wp:group {"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group"><!-- wp:post-excerpt /--></div>
 		<!-- /wp:group -->
@@ -26,7 +26,7 @@
 		<!-- /wp:spacer -->
 	<!-- /wp:post-template -->
 
-	<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
 		<!-- wp:query-pagination-previous /-->
 		<!-- wp:query-pagination-next /-->
 	<!-- /wp:query-pagination -->
