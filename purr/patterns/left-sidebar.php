0823a4a3c76bc99daab52d7124893868a9160cc7
sandro schutt <schuttsandro@outlook.com>
2024-03-17 23:43:06 -0300

    Fixes the two first tasks on issue #126

--- purr/patterns/left-sidebar.php
+++ purr/patterns/left-sidebar.php
@@ -1,22 +1,26 @@
 <?php
+
 /**
  * Title: Left Sidebar
  * Slug: purr/left-sidebar
  * Categories: sidebar
  */
 ?>
-<!-- wp:group {"style":{"position":{"type":""},"layout":{"selfStretch":"fixed","flexSize":"25%"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:0"><!-- wp:site-logo {"width":94,"className":"is-style-rounded"} /--></div>
-<!-- /wp:group -->
+<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"var:preset|custom|sidebar|width"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40)">
+        <!-- wp:site-logo {"className":"is-style-rounded"} /-->
+    </div>
+    <!-- /wp:group -->
 
-<!-- wp:heading -->
-<h2 class="wp-block-heading"><?php esc_html_e( 'About', 'purr' ); ?></h2>
-<!-- /wp:heading -->
+    <!-- wp:heading -->
+    <h2 class="wp-block-heading"><?php esc_html_e('About', 'purr'); ?></h2>
+    <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php esc_html_e( 'Purr is a theme for status updates.', 'purr' ); ?></p>
-<!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p><?php esc_html_e('Purr is a theme for status updates.', 'purr'); ?></p>
+    <!-- /wp:paragraph -->
 
-<!-- wp:archives {"showPostCounts":true} /--></div>
+    <!-- wp:archives {"showPostCounts":true} /-->
+</div>
 <!-- /wp:group -->
